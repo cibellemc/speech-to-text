c414c34dc14f14389e6248e08c2eeab3efd5c576
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2024-09-24 08:35:06 -0300

    fix: indentação

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,105 +59,103 @@ def upload_view():
 
         transcribe = st.form_submit_button(label="Iniciar")
 
-        # Se o usuário clicar em "Iniciar" e houver arquivos carregados, a transcrição será inicializada
-        if transcribe:
-            if input_files:
-                st.session_state.transcription = Transcription(
-                    input_files
-                )  # Uma classe que gerencia o processo de transcrição.
-                st.session_state.transcription.transcribe(
-                    whisper_model
-                )  # Função para realizar a transcrição com base no modelo selecionado.
-            else:
-                st.error("Por favor, selecione um arquivo.")
+    # Se o usuário clicar em "Iniciar" e houver arquivos carregados, a transcrição será inicializada
+    if transcribe:
+        if input_files:
+            st.session_state.transcription = Transcription(
+                input_files
+            )  # Uma classe que gerencia o processo de transcrição.
+            st.session_state.transcription.transcribe(
+                whisper_model
+            )  # Função para realizar a transcrição com base no modelo selecionado.
+        else:
+            st.error("Por favor, selecione um arquivo.")
 
         # Se houver uma transcrição, renderize-a. Caso contrário, exiba instruções
-        if "transcription" in st.session_state:
-            for i, output in enumerate(st.session_state.transcription.output):
-                doc = docx.Document()
-                avg_confidence_score = 0
-                amount_words = 0
-                save_dir = (
-                    str(pathlib.Path(__file__).parent.absolute()) + "/transcripts/"
-                )
+    if "transcription" in st.session_state:
+        for i, output in enumerate(st.session_state.transcription.output):
+            doc = docx.Document()
+            avg_confidence_score = 0
+            amount_words = 0
+            save_dir = (
+                str(pathlib.Path(__file__).parent.absolute()) + "/transcripts/"
+            )
 
-                # Verifica se o diretório existe, caso contrário, cria-o
-                if not os.path.exists(save_dir):
-                    os.makedirs(save_dir)
+            # Verifica se o diretório existe, caso contrário, cria-o
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
 
-                st.markdown(f"#### Transcrição de {output['name']}")
-                for idx, segment in enumerate(output["segments"]):
-                    for w in output["segments"][idx]["words"]:
-                        amount_words += 1
-                        avg_confidence_score += w["probability"]
+            st.markdown(f"#### Transcrição de {output['name']}")
+            for idx, segment in enumerate(output["segments"]):
+                for w in output["segments"][idx]["words"]: amount_words += 1
+                avg_confidence_score += w["probability"]
                 confidence_score = round(avg_confidence_score / amount_words, 3)
                 language = output["language"]
 
-                st.markdown(
-                    f"_(modelo Whisper:_`{whisper_model}` -  _idioma:_ `{language}` -  _⌀ índice de confiança:_ `{confidence_score}`)"
+            st.markdown(f"_(modelo Whisper:_`{whisper_model}` -  _idioma:_ `{language}` -  _⌀ índice de confiança:_ `{confidence_score}`)"
                 )
 
-                prev_word_end = -1
-                text = ""
-                html_text = ""
+            prev_word_end = -1
+            text = ""
+            html_text = ""
 
-                # cores de confiança - do vermelho ao verde
-                colors = [(0.6, 0, 0), (1, 0.7, 0), (0, 0.6, 0)]
-                cmap = mcolors.LinearSegmentedColormap.from_list("my_colormap", colors)
+            # cores de confiança - do vermelho ao verde
+            colors = [(0.6, 0, 0), (1, 0.7, 0), (0, 0.6, 0)]
+            cmap = mcolors.LinearSegmentedColormap.from_list("my_colormap", colors)
 
-                with st.expander("Transcrição"):
-                    color_coding = st.checkbox(
-                        "Codificação das cores",
-                        value=False,
-                        key={i},
-                        help="Codificar uma palavra por cores com base na probabilidade de ela ter sido reconhecida corretamente. A escala de cores varia de verde (alto) a vermelho (baixo).",
+            with st.expander("Transcrição"):
+                color_coding = st.checkbox(
+                    "Codificação das cores",
+                    value=False,
+                    key={i},
+                    help="Codificar uma palavra por cores com base na probabilidade de ela ter sido reconhecida corretamente. A escala de cores varia de verde (alto) a vermelho (baixo).",
+                )
+                for idx, segment in enumerate(output["segments"]):
+                    for w in output["segments"][idx]["words"]:
+
+                        if color_coding:
+                            rgba_color = cmap(w["probability"])
+                            rgb_color = tuple(
+                                round(x * 255) for x in rgba_color[:3]
+                            )
+                        else:
+                            rgb_color = (0, 0, 0)
+                        html_text += (
+                            f"<span style='color:rgb{rgb_color}'>{w['word']}</span>"
+                        )
+                        text += w["word"]
+                        # inserir uma quebra de linha se houver pontuação
+                        if any(c in w["word"] for c in "!?.") and not any(
+                            c.isdigit() for c in w["word"]
+                        ):
+                            html_text += "<br><br>"
+                            text += "\n\n"
+                st.markdown(html_text, unsafe_allow_html=True)
+            doc.add_paragraph(text)
+
+        # salvar a transcrição como docx no diretório local
+        file_name = (
+            output["name"]
+            + "-"
+            + whisper_model
+            + "-"
+            + datetime.today().strftime("%d-%m-%y")
+            + ".docx"
+        )
+        doc.save(save_dir + file_name)
+
+        # Salvar no banco de dados
+        save_transcription_to_db(file_name, text, language, confidence_score)
+
+        bio = io.BytesIO()
+        doc.save(bio)
+
+        st.download_button(
+                        label="Baixar Transcrição",
+                        data=bio.getvalue(),
+                        file_name=file_name,
+                        mime="docx",
                     )
-                    for idx, segment in enumerate(output["segments"]):
-                        for w in output["segments"][idx]["words"]:
-
-                            if color_coding:
-                                rgba_color = cmap(w["probability"])
-                                rgb_color = tuple(
-                                    round(x * 255) for x in rgba_color[:3]
-                                )
-                            else:
-                                rgb_color = (0, 0, 0)
-                            html_text += (
-                                f"<span style='color:rgb{rgb_color}'>{w['word']}</span>"
-                            )
-                            text += w["word"]
-                            # inserir uma quebra de linha se houver pontuação
-                            if any(c in w["word"] for c in "!?.") and not any(
-                                c.isdigit() for c in w["word"]
-                            ):
-                                html_text += "<br><br>"
-                                text += "\n\n"
-                    st.markdown(html_text, unsafe_allow_html=True)
-                    doc.add_paragraph(text)
-
-                # salvar a transcrição como docx no diretório local
-                file_name = (
-                    output["name"]
-                    + "-"
-                    + whisper_model
-                    + "-"
-                    + datetime.today().strftime("%d-%m-%y")
-                    + ".docx"
-                )
-                doc.save(save_dir + file_name)
-
-                # Salvar no banco de dados
-                save_transcription_to_db(file_name, text, language, confidence_score)
-
-                bio = io.BytesIO()
-                doc.save(bio)
-
-                st.download_button(
-                                label="Baixar Transcrição",
-                                data=bio.getvalue(),
-                                file_name=file_name,
-                                mime="docx",
-                            )
 
 
 def main():
